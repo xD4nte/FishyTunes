4e3da103dfdaa6b7caaeced602ca5d855b981e21
Laura Sawilla <xd4nte@gmx.de>
2025-01-25 12:09:48 +0100

    Player
    Player Scene & initial scripts

---
+++ fishytunes_ggj25/Assets/PlayerMovement.gd
@@ -0,0 +1,13 @@
+extends Node
+var move_speed = 1000
+@export var player_body : CharacterBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+	player_body.velocity = input * move_speed
+	player_body.move_and_slide()
