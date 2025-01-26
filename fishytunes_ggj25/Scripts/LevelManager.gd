b5b1f6f4bc4846b20d43005e30848f72d5999df4
jibLord <155445673+jibLord@users.noreply.github.com>
2025-01-26 16:22:45 +0100

    offset

--- fishytunes_ggj25/Scripts/LevelManager.gd
+++ fishytunes_ggj25/Scripts/LevelManager.gd
@@ -14,8 +14,8 @@ var is_transitioning = false
 @onready var player = $"../PlayerLayer/Player"
 
 func _ready():
-	bubble_manager.connect("level_completed", _on_level_completed)
-	connect("game_won", _on_game_won)
+	bubble_manager.connect("level_completed", Callable(self, "_on_level_completed"))
+	self.connect("game_won", Callable(self, "_on_game_won"))
 	start_game()
 
 func start_game():
@@ -33,23 +33,33 @@ func reset_player():
 
 func load_current_level():
 	is_transitioning = true
-	await transition.fade_in()
+	transition.fade_in()
 	reset_player()
 	bubble_manager.load_level(current_level)
-	await transition.fade_out()
+	transition.fade_out()
 	is_transitioning = false
 	bubble_manager.highlight_correct_bubbles()
 
 func _on_level_completed():
 	var level_number = int(current_level.split("_")[1])
-	if(level_number == 1):
-		Melody_Lvl1.play()
-	elif(level_number == 2):
-		Melody_Lvl2.play()
+	var melody_player = null
+
+	if level_number == 1:
+		melody_player = Melody_Lvl1
+	elif level_number == 2:
+		melody_player = Melody_Lvl2
+
+	if melody_player:
+		melody_player.play()
+		melody_player.connect("finished", Callable(self, "_on_melody_finished"))
+
 	is_transitioning = true
+
+func _on_melody_finished():
+	var level_number = int(current_level.split("_")[1])
+
 	if level_number == max_level:
 		emit_signal("game_won")
-		_on_game_won()
 	else:
 		current_level = "level_%d" % (level_number + 1)
 		load_current_level()
